231c59a13921e1d50b8f13bd7f0aeee5a78ec2e5
Yixiao Li, Macbook Air <yxli2123@gmail.com>
2023-06-29 23:09:03 -0400

    get

--- dequantize_weight.py
+++ dequantize_weight.py
@@ -4,6 +4,7 @@ import torch
 from peft import LoraConfig, get_peft_model
 import argparse
 from falcontune.model import lora
+from transformers import AutoModelForCausalLM
 
 
 def find_4bit_layers(module, layers=[lora.Linear4bitLt], name=''):
@@ -27,14 +28,15 @@ def svd_init(module, names, bits, groupsize, quantlinear_class, name=''):
             setattr(module, attr, quantlinear_class(bits, groupsize, tmp.in_features, tmp.out_features))
 
     for name1, child in module.named_children():
-        replace_4bit_linear(child, names, bits, groupsize, quantlinear_class,
+        svd_init(child, names, bits, groupsize, quantlinear_class,
                             name + '.' + name1 if name != '' else name1)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', type=str, default='falcon-7b-instruct-4bit')
-    parser.add_argument('--falcon_ckpt', type=str, default='gptq_model-4bit-64g.safetensors')
+    parser.add_argument('--falcon_ckpt_q', type=str, default='gptq_model-4bit-64g.safetensors')
+    parser.add_argument('--falcon_ckpt_f', type=str, default='tiiuae/falcon-7b')
     parser.add_argument('--backend', type=str, default='torch')
 
     parser.add_argument("--lora_r", default=8, type=int, help="Default: %(default)s")
@@ -46,7 +48,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     falcon, tokenizer = load_model(model_name=args.model_name,
-                                   weights=args.falcon_ckpt,
+                                   weights=args.falcon_ckpt_q,
                                    backend=args.backend,
                                    half=False)
 
@@ -60,11 +62,19 @@ if __name__ == '__main__':
         task_type="CAUSAL_LM",
     )
 
-    model = get_peft_model(falcon, lora_config)
-    print(falcon)
-    print(model)
-    for falcon_name, lora_name in zip(falcon.state_dict().keys(), model.state_dict().keys()):
-        print(falcon_name, lora_name)
+    dmodel = get_peft_model(falcon, lora_config)
+
+    fmodel = AutoModelForCausalLM.from_pretrained(args.falcon_ckpt_f,
+                                                  device_map='auto',
+                                                  torch_dtype=torch.float,
+                                                  trust_remote_code=True)
+
+    print(dmodel)
+    print(fmodel)
+
+    print(dmodel.state_dict().keys())
+    print("======================================")
+    print(fmodel.state_dict().keys(), len(fmodel.state_dict().keys()))
 
 
     # svd_init(
